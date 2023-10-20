11ba9057cf3a677d0b65cd899711035fbc106a58
Miguel Angel Medina <mediagui@gmail.com>
2023-10-20 10:30:30 +0200

    chore: Se optimizan imports

--- boot/src/main/java/com/inatlas/config/CommandLineConfiguration.java
+++ boot/src/main/java/com/inatlas/config/CommandLineConfiguration.java
@@ -1,15 +1,10 @@
 package com.inatlas.config;
 
-import com.inatlas.domain.usecase.command.Command;
 import com.inatlas.domain.usecase.command.Invoker;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
-
-import java.util.Map;
 
 @Configuration
  @Profile("command-line-runner")
