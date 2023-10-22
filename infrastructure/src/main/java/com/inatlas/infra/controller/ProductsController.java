28e40b9bf45e94ad8332a6a1018e7de845e48ac0
Miguel Angel Medina <mediagui@gmail.com>
2023-10-22 18:07:23 +0200

    feat: Implementación del controlador de productos

--- infrastructure/src/main/java/com/inatlas/infra/controller/ProductsController.java
+++ infrastructure/src/main/java/com/inatlas/infra/controller/ProductsController.java
@@ -1,40 +1,33 @@
 package com.inatlas.infra.controller;
 
+import com.inatlas.infra.api.dto.ResponseDTO;
+import com.inatlas.infra.api.product.ProductsControllerApi;
 import com.inatlas.infra.service.ProductsService;
-import com.inatlas.infra.api.ApiCoffeeShopController;
-import com.inatlas.infra.api.ApiCoffeeShopDelegate;
-import com.inatlas.infra.dto.OrderDTO;
-import com.inatlas.infra.dto.ResponseDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
 
-public class ProductsController extends ApiCoffeeShopController {
+@Controller
+public class ProductsController  implements ProductsControllerApi {
 
-  private ProductsService delegate;
+  private final ProductsService productsService;
 
-  public ProductsController(ApiCoffeeShopDelegate delegate) {
-    super(delegate);
+
+  public ProductsController(ProductsService productsService) {
+      this.productsService = productsService;
+
   }
 
-  @Override
-  public ResponseEntity<OrderDTO> getActualOrder() {
-    return super.getActualOrder();
-  }
 
   @Override
   public ResponseEntity<ResponseDTO> getAllProducts() {
-    return super.getAllProducts();
+    return productsService.getAllProducts();
   }
 
   @Override
   public ResponseEntity<ResponseDTO> getProductById(Integer id) {
-    return super.getProductById(id);
+    return productsService.getProductById(id);
   }
 
 
-
-  @Override
-  public ResponseEntity<OrderDTO> getReceipt(String format) {
-    return super.getReceipt(format);
-  }
 }
