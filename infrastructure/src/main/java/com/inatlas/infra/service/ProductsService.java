7467ef2f8345f9f38696810eecadae13f80988eb
Miguel Angel Medina <mediagui@gmail.com>
2023-10-23 09:10:17 +0200

    chore: Documentación técnica

--- infrastructure/src/main/java/com/inatlas/infra/service/ProductsService.java
+++ infrastructure/src/main/java/com/inatlas/infra/service/ProductsService.java
@@ -15,49 +15,60 @@ import java.util.Optional;
 
 import static java.util.Collections.EMPTY_LIST;
 
+/**
+ * Service class that handles operations related to products.
+ */
 @Service
-public class ProductsService  {
+public class ProductsService {
 
   private final GetOneProductUseCase getOneProductUseCase;
   private final GetAllProductsUseCase getAllProductsUseCase;
   private final ProductDTOMapper productDTOMapper;
-
   private ResponseDTO responseDTO;
 
+  /**
+   * Constructs an instance of ProductsService with the provided dependencies.
+   *
+   * @param getOneProductUseCase The use case for retrieving a single product.
+   * @param getAllProductsUseCase The use case for retrieving all products.
+   * @param productDTOMapper The mapper used to convert Product objects to DTOs.
+   */
   public ProductsService(GetOneProductUseCase getOneProductUseCase, GetAllProductsUseCase getAllProductsUseCase, ProductDTOMapper productDTOMapper) {
     this.getOneProductUseCase = getOneProductUseCase;
     this.getAllProductsUseCase = getAllProductsUseCase;
     this.productDTOMapper = productDTOMapper;
   }
 
-  
-
-
+  /**
+   * Retrieves all products.
+   *
+   * @return A ResponseEntity containing the response details as a ResponseDTO.
+   */
   public ResponseEntity<ResponseDTO> getAllProducts() {
-
-
-    List<Product> products = getAllProductsUseCase.getAllProducts().orElseGet(() -> EMPTY_LIST);
-
+    List<Product> products = getAllProductsUseCase.getAllProducts().orElseGet(Collections::emptyList);
     HttpStatus status = products.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK;
 
     // We make sure that the response is always a new object
     responseDTO = new ResponseDTO();
     responseDTO.setProducts(productDTOMapper.toDTOList(products));
-    return ResponseEntity.status(status).body(responseDTO);
 
+    return ResponseEntity.status(status).body(responseDTO);
   }
 
+  /**
+   * Retrieves a product by its ID.
+   *
+   * @param id The ID of the product to retrieve.
+   * @return A ResponseEntity containing the response details as a ResponseDTO.
+   */
   public ResponseEntity<ResponseDTO> getProductById(Integer id) {
-
     Optional<Product> product = getOneProductUseCase.getTheProduct(id);
-
     HttpStatus status = product.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND;
 
     // We make sure that the response is always a new object
     responseDTO = new ResponseDTO();
     responseDTO.setProducts(Collections.singletonList(productDTOMapper.toDTO(product.orElse(null))));
+
     return ResponseEntity.status(status).body(responseDTO);
-
   }
-
 }
--- infrastructure/src/main/java/com/inatlas/infra/service/ReceiptService.java
+++ infrastructure/src/main/java/com/inatlas/infra/service/ReceiptService.java
@@ -3,7 +3,6 @@ package com.inatlas.infra.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.inatlas.domain.db.mapper.OrderDTOMapper;
 import com.inatlas.domain.entity.Order;
 import com.inatlas.domain.usecase.FindLastOrderCompletedUseCase;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -22,62 +21,81 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
 
+/**
+ * Service class that handles operations related to generating receipts.
+ */
 @Service
 public class ReceiptService {
 
   private FindLastOrderCompletedUseCase findLastOrderCompletedUseCase;
 
-  public ReceiptService(FindLastOrderCompletedUseCase findLastOrderCompletedUseCase, ReceiptService receiptService, OrderDTOMapper orderDTOMapper) {
-
+  /**
+   * Constructs an instance of ReceiptService with the provided dependencies.
+   *
+   * @param findLastOrderCompletedUseCase The use case for finding the last completed order.
+   */
+  public ReceiptService(FindLastOrderCompletedUseCase findLastOrderCompletedUseCase) {
     this.findLastOrderCompletedUseCase = findLastOrderCompletedUseCase;
   }
 
+  /**
+   * Retrieves the receipt in the specified format.
+   *
+   * @param format The format of the receipt (e.g., "pdf").
+   * @return A ResponseEntity containing the receipt as a Resource.
+   * @throws IOException If an I/O error occurs while generating the receipt.
+   */
   public ResponseEntity<Resource> getReceipt(String format) throws IOException {
-
-
     final Optional<Order> lastOrderCompleted = findLastOrderCompletedUseCase.getLastOrderCompleted();
 
     if (lastOrderCompleted.isEmpty()) {
       return ResponseEntity.notFound().build();
     }
 
-    // Genera el archivo PDF
+    // Generate the PDF file
     byte[] bytes = generatePDFReceipt(lastOrderCompleted);
 
-    // Crea un recurso con el contenido del archivo PDF
+    // Create a resource with the content of the PDF file
     Resource resource = new ByteArrayResource(bytes);
 
-    // Devuelve el recurso con el tipo de contenido correspondiente
+    // Return the resource with the corresponding content type
     if ("pdf".equals(format)) {
       return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(resource);
     } else {
       return ResponseEntity.badRequest().build();
     }
-
   }
 
+  /*
+   * Generates a PDF receipt for the provided order.
+   * @param lastOrderCompleted The order for which to generate the receipt.
+   * @return The generated PDF receipt.
+   */
   private byte[] generatePDFReceipt(Optional<Order> lastOrderCompleted) throws IOException {
     Order order;
+
     if (lastOrderCompleted.isPresent()) {
       order = lastOrderCompleted.get();
-
       ObjectMapper mapper = new ObjectMapper();
       mapper.registerModule(new JavaTimeModule());
-
       mapper.enable(SerializationFeature.INDENT_OUTPUT);
       String json = mapper.writeValueAsString(order);
       String[] lines = json.split(System.lineSeparator());
-      return getGeneratedPDF(lines);
-
+      return writePDF(lines);
     }
+
     return null;
   }
 
-  private byte[] getGeneratedPDF(String[] lines) throws IOException {
+  /*
+   * Write a PDF receipt in the System.getProperty("java.io.tmpdir") folder.
+   * @param lines The lines to include in the receipt.
+   * @return The generated PDF receipt.
+   */
+  private byte[] writePDF(String[] lines) throws IOException {
     PDDocument document = new PDDocument();
     PDPage page = new PDPage();
     document.addPage(page);
-
     PDPageContentStream contentStream = new PDPageContentStream(document, page);
     contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.COURIER), 12);
     contentStream.beginText();
@@ -91,14 +109,7 @@ public class ReceiptService {
 
     contentStream.endText();
     contentStream.close();
-
     document.save(System.getProperty("java.io.tmpdir") + "/receipt.pdf");
-
-
     return Files.readAllBytes(Paths.get(System.getProperty("java.io.tmpdir") + "receipt.pdf"));
-
-
   }
-
-
 }
