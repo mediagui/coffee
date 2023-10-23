3020eefee7047beee8bb93409dc2fd93890e0e25
Miguel Angel Medina <mediagui@gmail.com>
2023-10-23 16:30:27 +0200

    test: Tests sobre servicio de operaciones

---
+++ infrastructure/src/test/java/com/inatlas/infra/service/OrderServiceTest.java
@@ -0,0 +1,79 @@
+package com.inatlas.infra.service;
+
+import com.inatlas.domain.db.mapper.OrderDTOMapper;
+import com.inatlas.domain.entity.Order;
+import com.inatlas.domain.usecase.OperationsOnOrderUseCase;
+import com.inatlas.domain.usecase.PayOrderUseCase;
+import com.inatlas.infra.api.dto.OrderDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.inatlas.infra.service.CoffeeTestUtil.*;
+import static com.inatlas.infra.service.CoffeeTestUtil.generateOrderDTO;
+import static com.inatlas.infra.service.CoffeeTestUtil.generateOrderItemList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class OrderServiceTest {
+  @Mock
+  OrderDTOMapper orderDTOMapper;
+  @Mock
+  OperationsOnOrderUseCase operationsOnOrderUseCase;
+  @InjectMocks
+  OrderService orderService;
+  AutoCloseable closeable;
+
+  @BeforeEach
+  void setUp() {
+    closeable = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    closeable.close();
+  }
+
+
+  @Test
+  public void testAddProductToOrder() {
+
+    Integer productId = 1;
+    Integer amount = 1;
+    OrderDTO orderDTO = new OrderDTO(List.of(generateDrinkOrderItemDTO()),1d);
+    Order order = new Order(List.of(generateDrinkOrderItem()));
+    order.setTotal(1d);
+
+    when(operationsOnOrderUseCase.addProductToOrder(productId, amount)).thenReturn(Optional.of(order));
+    when(orderDTOMapper.toDTO(order)).thenReturn(orderDTO);
+
+    ResponseEntity<OrderDTO> response = orderService.addProductToOrder(productId, amount);
+
+    assertEquals(orderDTO, response.getBody());
+  }
+
+  @Test
+  public void testPayOrder() {
+
+    OrderDTO orderDTO = generateOrderDTO();
+
+    when(orderDTOMapper.toDTO(any(Order.class))).thenReturn(orderDTO);
+    when(operationsOnOrderUseCase.payOrder()).thenReturn(Optional.of(generateCompletedOrder()));
+
+    ResponseEntity<OrderDTO> response = orderService.payOrder();
+
+
+    assertEquals(orderDTO, response.getBody());
+  }
+}
+
+
