88b4333d63c7067ec72830b27478889495ecd5be
Miguel Angel Medina <mediagui@gmail.com>
2023-10-24 10:57:28 +0200

    feat: Nuevo mapper para promociones

---
+++ infrastructure/src/main/java/com/inatlas/domain/db/mapper/PromotionDTOMapper.java
@@ -0,0 +1,21 @@
+/**
+ * This is a mapper interface for converting between Product and ProductDB entities.
+ * It uses MapStruct library and is annotated with @Mapper and @ComponentModel.
+ * The mapper interface extends AbstractBBDDMapper  interface.
+ * It provides mapping methods for converting Product objects to ProductDB objects and vice versa.
+ * The componentModel attribute is set to "spring" to enable Spring component model.
+ */
+package com.inatlas.domain.db.mapper;
+
+import com.inatlas.domain.entity.Order;
+import com.inatlas.domain.entity.Promotion;
+import com.inatlas.infra.api.dto.OrderDTO;
+import com.inatlas.infra.api.dto.PromotionDTO;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring",
+        uses = OrderItemDTOMapper.class,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface PromotionDTOMapper extends AbstractDTOMapper<Promotion, PromotionDTO> {
+}
