da69f4d2aa12105c64d266e2fa58dcfc9f0b7c09
Miguel Angel Medina <mediagui@gmail.com>
2023-10-24 11:00:28 +0200

    feat: Se añade configuración de activar repositorios jpa

---
+++ boot/src/main/java/com/inatlas/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.inatlas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.inatlas")
+public class JpaConfig {
+}
